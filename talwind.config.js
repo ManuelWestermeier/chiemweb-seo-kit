35e8a3614a8f7616d0be623b571727dffcd20863
ManuelWestermeier <westermeier111@gmail.com>
2026-06-14 14:36:44 +0200

    build

---
+++ talwind.config.js
@@ -0,0 +1,7 @@
+export default {
+    content: [
+        "./index.html",
+        "./artikel/**/*.html",
+        "./src/**/*.{js,ts,jsx,tsx}",
+    ],
+}
--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,11 @@
 // vite.config.js
 import { defineConfig } from "vite";
 import { resolve } from "path";
+import tailwindcss from '@tailwindcss/vite'
+
 
 export default defineConfig({
+  plugins: [tailwindcss()],
   build: {
     outDir: "docs",
     rollupOptions: {
